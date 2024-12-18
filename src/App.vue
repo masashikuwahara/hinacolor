c33f33c9117350774fc21b3f901dc954288765bd
masashikuwahara <brucemclarenp4@gmail.com>
2024-12-18 23:27:28 +0900

    メンバーの画像を変更

--- src/App.vue
+++ src/App.vue
@@ -23,34 +23,34 @@ export default {
       const combination = selectedColors.sort().join('-');
       const combinations = {
         'ブルー-ブルー': { name: '加藤史帆', img: 'https://cdn.hinatazaka46.com/images/14/610/d713c2e65eee8fa173ef6c62d0d49/800_800_102400.jpg' },
-        'パステルブルー-パープル': { name: '佐々木久美', img: 'https://cdn.hinatazaka46.com/images/14/ee4/c440e836d7b07df8aefc0b84ab181/800_800_102400.jpg' },
-        'イエロー-イエロー': { name: '佐々木美玲', img: 'https://cdn.hinatazaka46.com/images/14/235/7c13d7b8915d31edbe0325581728d/800_800_102400.jpg' },
-        'ピンク-ホワイト': { name: '高瀬愛奈', img: 'https://cdn.hinatazaka46.com/images/14/192/fd8942dc6d3234f5e3218201955a9/800_800_102400.jpg' },
+        'パステルブルー-パープル': { name: '佐々木久美', img: 'https://cdn.hinatazaka46.com/images/14/4f1/2d700f34ba47f7c556c4f9b0080b5/800_800_102400.jpg' },
+        'イエロー-イエロー': { name: '佐々木美玲', img: 'https://cdn.hinatazaka46.com/images/14/0df/18fcfe112bdc514260a599f15ca6d/800_800_102400.jpg' },
+        'ピンク-ホワイト': { name: '高瀬愛奈', img: 'https://cdn.hinatazaka46.com/images/14/ccb/7cc864c9f0c54e8d7cc27c88cd6a1/800_800_102400.jpg' },
         'サクラピンク-サクラピンク': { name: '東村芽依', img: 'https://cdn.hinatazaka46.com/images/14/e8d/6d3443d32d21af5b4250cae4dfe98/800_800_102400.jpg' },
-        'イエロー-パステルブルー': { name: '金村美玖', img: 'https://cdn.hinatazaka46.com/images/14/884/f4b546a892ac328b9c50c546e9a8b/800_800_102400.jpg' },
-        'イエロー-サクラピンク': { name: '河田陽菜', img: 'https://cdn.hinatazaka46.com/images/14/bfe/73bebf9ac0ba7d4b4aad96a2cea80/800_800_102400.jpg' },
-        'バイオレット-ホワイト': { name: '小坂菜緒', img: 'https://cdn.hinatazaka46.com/images/14/4ef/7eac8ec151107a9de955beaf7329a/800_800_102400.jpg' },
-        'パープル-パープル': { name: '富田鈴花', img: 'https://cdn.hinatazaka46.com/images/14/f4c/60526d1d91e8d440a5e8c9047fc0e/400_320_102400.jpg' },
+        'イエロー-パステルブルー': { name: '金村美玖', img: 'https://cdn.hinatazaka46.com/images/14/e3d/10e0ec44b93be391c4ab7f7e3cf0b/400_320_102400.jpg' },
+        'イエロー-サクラピンク': { name: '河田陽菜', img: 'https://cdn.hinatazaka46.com/images/14/45b/c47ef916e2e0bf5e49a0144179153/400_320_102400.jpg' },
+        'バイオレット-ホワイト': { name: '小坂菜緒', img: 'https://cdn.hinatazaka46.com/images/14/985/072c23fa8e0ac974f353d51d6d566/400_320_102400.jpg' },
+        'パープル-パープル': { name: '富田鈴花', img: 'https://cdn.hinatazaka46.com/images/14/42f/2e3b0b500456d48a46514eceb343d/400_320_102400.jpg' },
         'オレンジ-オレンジ': { name: '丹生明里', img: 'https://cdn.hinatazaka46.com/images/14/9bd/04f076fd4bb2a2133af488e5a4732/400_320_102400.jpg' },
         'オレンジ-ホワイト': { name: '濱岸ひより', img: 'https://cdn.hinatazaka46.com/images/14/afc/00d2a3192ea32e11b16094ab3eff5/400_320_102400.jpg' },
-        'サクラピンク-パールグリーン': { name: '松田好花', img: 'https://cdn.hinatazaka46.com/images/14/eb4/2dfbfe8d46a69460fc9f16888acb4/400_320_102400.jpg' },
-        'エメラルドグリーン-レッド': { name: '上村ひなの', img: 'https://cdn.hinatazaka46.com/images/14/f93/c946abc18313af6ecabf728050f4b/400_320_102400.jpg' },
-        'グリーン-パープル': { name: '髙橋未来虹', img: 'https://cdn.hinatazaka46.com/images/14/eb8/35660b17030be73ffbbfb41555b72/400_320_102400.jpg' },
-        'オレンジ-ブルー': { name: '森本茉莉', img: 'https://cdn.hinatazaka46.com/images/14/937/25b018046acd1e646e856c6369e9b/400_320_102400.jpg' },
-        'イエロー-パールグリーン': { name: '山口陽世', img: 'https://cdn.hinatazaka46.com/images/14/bcc/cfb92e43a263000b242439e485fd0/400_320_102400.jpg' },
-        'オレンジ-サクラピンク': { name: '石塚瑶季', img: 'https://cdn.hinatazaka46.com/images/14/a86/e1f789a190060837b09fa82076e2a/800_800_102400.jpg' },
-        'パープル-ブルー': { name: '小西夏菜実', img: 'https://cdn.hinatazaka46.com/images/14/eaf/3b49b29c292f09ed71bb8649a9e12/400_320_102400.jpg' },
-        'オレンジ-パールグリーン': { name: '清水理央', img: 'https://cdn.hinatazaka46.com/images/14/74d/4b397480fd172844cbcc5bacfe5e7/400_320_102400.jpg' },
-        'オレンジ-レッド': { name: '正源司陽子', img: 'https://cdn.hinatazaka46.com/images/14/f02/6406ed92a0d1a7769fc2561dfcc68/400_320_102400.jpg' },
-        'イエロー-レッド': { name: '竹内希来里', img: 'https://cdn.hinatazaka46.com/images/14/f81/9e69fa3e1130d66af7f867156616b/400_320_102400.jpg' },
-        'オレンジ-パステルブルー': { name: '平尾帆夏', img: 'https://cdn.hinatazaka46.com/images/14/660/2cf7f6f78adf856981934c3dc479e/400_320_102400.jpg' },
-        'イエロー-ブルー': { name: '平岡海月', img: 'https://cdn.hinatazaka46.com/images/14/71d/a3bcbe403190ec3c9725084518ee3/400_320_102400.jpg' },
-        'サクラピンク-ブルー': { name: '藤嶌果歩', img: 'https://cdn.hinatazaka46.com/images/14/0a7/035fb647cd649e349d4fa75d4184e/400_320_102400.jpg' },
-        'バイオレット-レッド': { name: '宮地すみれ', img: 'https://cdn.hinatazaka46.com/images/14/c33/92e4f5413e4613cd436b978e4e56a/400_320_102400.jpg' },
-        'エメラルドグリーン-ホワイト': { name: '山下葉留花', img: 'https://cdn.hinatazaka46.com/images/14/ec6/d2b4bb358013dc930382899d74f98/400_320_102400.jpg' },
-        'パープル-ピンク': { name: '渡辺莉奈', img: 'https://cdn.hinatazaka46.com/images/14/d33/b748daecd841a480ba4dce08eca48/400_320_102400.jpg' }
+        'サクラピンク-パールグリーン': { name: '松田好花', img: 'https://cdn.hinatazaka46.com/images/14/9c5/87d50e7853595aaefc3d14ab80749/400_320_102400.jpg' },
+        'エメラルドグリーン-レッド': { name: '上村ひなの', img: 'https://cdn.hinatazaka46.com/images/14/9de/8d4bfed193adf56953f7aa3625760/400_320_102400.jpg' },
+        'グリーン-パープル': { name: '髙橋未来虹', img: 'https://cdn.hinatazaka46.com/images/14/f8f/4112a6932e8751737def6f738abee/400_320_102400.jpg' },
+        'オレンジ-ブルー': { name: '森本茉莉', img: 'https://cdn.hinatazaka46.com/images/14/65f/6ca2ac061bcda8026fb0a5aa9c804/400_320_102400.jpg' },
+        'イエロー-パールグリーン': { name: '山口陽世', img: 'https://cdn.hinatazaka46.com/images/14/ebc/846dca80d22fbdb2dbf42f24ff5d9/400_320_102400.jpg' },
+        'オレンジ-サクラピンク': { name: '石塚瑶季', img: 'https://cdn.hinatazaka46.com/images/14/b56/49ce8fef3670b03d97ed6b4212486/400_320_102400.jpg' },
+        'パープル-ブルー': { name: '小西夏菜実', img: 'https://cdn.hinatazaka46.com/images/14/7af/4b0f4d4f5c906d391afea8edc8d25/400_320_102400.jpg' },
+        'オレンジ-パールグリーン': { name: '清水理央', img: 'https://cdn.hinatazaka46.com/images/14/b00/62d7047d19b84cc1b2922e011e2bf/400_320_102400.jpg' },
+        'オレンジ-レッド': { name: '正源司陽子', img: 'https://cdn.hinatazaka46.com/images/14/9b4/914eced5b352a11d009eafedfefbc/400_320_102400.jpg' },
+        'イエロー-レッド': { name: '竹内希来里', img: 'https://cdn.hinatazaka46.com/images/14/66d/c4f27cba509510a6f48c23a9ece1c/400_320_102400.jpg' },
+        'オレンジ-パステルブルー': { name: '平尾帆夏', img: 'https://cdn.hinatazaka46.com/images/14/1de/576523c0628298356025a2193ecb0/400_320_102400.jpg' },
+        'イエロー-ブルー': { name: '平岡海月', img: 'https://cdn.hinatazaka46.com/images/14/c86/d4f17e3172581f7855fd835f89f90/400_320_102400.jpg' },
+        'サクラピンク-ブルー': { name: '藤嶌果歩', img: 'https://cdn.hinatazaka46.com/images/14/e39/7a5d6767ae33af1a7319529c461b5/400_320_102400.jpg' },
+        'バイオレット-レッド': { name: '宮地すみれ', img: 'https://cdn.hinatazaka46.com/images/14/fa2/dbc520dc2a261472c591987a0a3ae/400_320_102400.jpg' },
+        'エメラルドグリーン-ホワイト': { name: '山下葉留花', img: 'https://cdn.hinatazaka46.com/images/14/f25/16be8f588b8407199f992e5721915/400_320_102400.jpg' },
+        'パープル-ピンク': { name: '渡辺莉奈', img: 'https://cdn.hinatazaka46.com/images/14/249/c9216245dc6e1bb429ca60ef175bb/400_320_102400.jpg' }
       };
-      this.result = combinations[combination] || { name: '組み合わせ無し', img: 'https://cdn.hinatazaka46.com/images/14/760/4b421a6dd5d6e1f278f3651a39723/200_200_102400.jpg' };
+      this.result = combinations[combination] || { name: '組み合わせ無し', img: 'https://cdn.hinatazaka46.com/images/14/cee/fdd9ce5600c3e28449d817494f466/200_200_102400.jpg' };
     }
   }
 };
