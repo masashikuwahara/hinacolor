32182e65c3699787222dcecfbeb2df1d0b5d7304
test <brucemclarenp4@gmail.com>
2025-04-08 21:52:04 +0900

    update

--- src/components/ResultDisplay.vue
+++ src/components/ResultDisplay.vue
@@ -9,7 +9,7 @@
     {{ history.date }} {{ history.content }}
   </div>
   <p class="description">
-    ※ペンライトカラー及びメンバーは2025年1月現在の情報です
+    ※ペンライトカラー及びメンバーは2025年4月現在の情報です
   </p>
   <br>
   Ver.1.2.1
