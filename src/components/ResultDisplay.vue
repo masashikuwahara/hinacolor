e66af5c8054826bcb6a7effd404c7f1ea9a286cb
test <brucemclarenp4@gmail.com>
2025-05-09 00:37:11 +0900

    履歴更新

--- src/components/ResultDisplay.vue
+++ src/components/ResultDisplay.vue
@@ -9,10 +9,10 @@
     {{ history.date }} {{ history.content }}
   </div>
   <p class="description">
-    ※ペンライトカラー及びメンバーは2025年4月現在の情報です
+    ※ペンライトカラー及びメンバーは2025年5月現在の情報です
   </p>
   <br>
-  Ver.1.2.1
+  Ver.1.2.2
 </template>
 
 <script>
@@ -20,10 +20,13 @@ export default {
   data(){
     return {
       histories: [
+        { date: '2025.05.09', content: 'メンバー画像を変更'},
+        { date: '2025.05.09', content: '五期生を追加'},
+        { date: '2025.05.09', content: '高瀬愛奈を削除'},
         { date: '2025.04.11', content: '佐々木久美、佐々木美玲を削除'},
         { date: '2025.04.08', content: '清水理央と渡辺莉奈のカラー変更'},
         { date: '2025.03.13', content: '選択した色を表示する機能の追加'},
-        { date: '2024.12.18', content: 'リリースしました'}
+        { date: '2024.12.18', content: 'リリース'}
       ],
     }
   },
